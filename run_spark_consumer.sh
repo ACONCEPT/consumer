a89666d30cc0ee3ae814f7b769bb05fa4af74f99
Ubuntu <ubuntu@ip-172-31-31-189.us-west-2.compute.internal>
2018-05-01 19:29:14 +0000

    spark consumer pushing to kafka

---
+++ run_spark_consumer.sh
@@ -0,0 +1,2 @@
+/usr/local/spark/bin/spark-submit --master spark://50.112.50.75:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 spark_consumer.py
+/usr/local/spark/bin/spark-submit --master spark://50.112.50.75:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 spark_consumer.py
--- spark_consumer.py
+++ spark_consumer.py
@@ -1,27 +1,40 @@
 import os
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 pyspark-shell'
-import numpy as np
+
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-from pyspark.sql import Row, SparkSession
-from pyspark.sql.window import Window
-from pyspark.sql.functions import rank, col
-from insight_project.config import BOOTSTRAP_SERVERS,ZOOKEEPER_SERVERS
+from config import BOOTSTRAP_SERVERS,ZOOKEEPER_SERVERS
 import json, math, datetime
+from kafka import KafkaProducer
+producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS,\
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"))
+
+def handler(message):
+    records = message.collect()
+    for record in records:
+        producer.send("sparkout",str(record))
+        producer.flush()
+
+class KSWorker(object):
+    def __init__(self):
+        self.sc = SparkContext(appName="PythonSparkStreamingKafka")
+        self.sc.setLogLevel("WARN")
+        self.ssc = StreamingContext(self.sc,10)
+
+    def startStream(self):
+        self.ssc.start()
+        self.ssc.awaitTermination()
+
+    def testStream(self):
+        kafkaStream = KafkaUtils.createDirectStream(self.ssc, ['test'], {"metadata.broker.list":",".join(BOOTSTRAP_SERVERS)})
+        data_ds = kafkaStream.map(lambda v: json.loads(v[1]))
+        data_ds.foreachRDD(handler)
+        self.startStream()
 
 def main():
-    sc = SparkContext(appName="PythonSparkStreamingKafka")
-    sc.setLogLevel("WARN")
-    ssc = StreamingContext(sc)
-    kafkaStream = KafkaUtils.createDirectStream(ssc, ['test'], {"metadata.broker.list":BOOTSTRAP_SERVERS})
-    data_ds = kafkaStream.map(lambda v: json.loads(v[1]))
-    data_ds.show()
-    ssc.start()
-    ssc.awaitTermination()
-
+    worker = KSWorker()
+    worker.testStream()
 
 if __name__ == '__main__':
-
     main()
-
