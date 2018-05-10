ce6f948f8491d3ea6ca9b06b6e9fccbc8dd8f383
ACONCEPT <joesadaka@gmail.com>
2018-05-10 06:52:22 -0500

    updated rule

--- spark_func.py
+++ spark_func.py
@@ -23,7 +23,6 @@ def getSparkSessionInstance(sparkConf):
 
 def stream_validation(bootstrap_servers,datasource,table,validation_config):
     sc = SparkContext(appName="PythonSparkStreamingKafka")
-#    sc.addPyFile("{}/config/config.py".format(os.environ["PROJECT_HOME"]))
     sqlc = SQLContext(sc)
     ssc = StreamingContext(sc,5)
     jdbc_url , jdbc_properties = get_url(datasource)
@@ -45,94 +44,66 @@ def stream_validation(bootstrap_servers,datasource,table,validation_config):
     brokerlist = ",".join(bootstrap_servers)
     kafka_properties = {}
     kafka_properties["metadata.broker.list"] = brokerlist
-
     producer.produce_debug("intiializing stream on topic {}".format(topic))
     kafkaStream = KafkaUtils.createDirectStream(ssc,\
                                                 [topic],\
                                                 kafka_properties)
+    dependencies = {}
+    for rule in validation_config:
+        dependencies[rule.name] = []
+        for d in rule.dependencies:
+            producer.produce_debug("getting dependency for {}".format(d))
+            name = rule.name
+            dependencies[rule.name].append(get_table_df(d))
 
     #decode json to ds
     data_ds = kafkaStream.map(lambda v: json.loads(v[1]))
     data_ds.count().map(lambda x:'Records in this batch: %s' % x).union(data_ds).pprint()
-    rule = validation_config[0]
 
+    #hardcoded which validation functions are active
+    validation_functions = {"check_exists":check_exists}
 
     def check_exists(time,rdd):
-        rdd  = rdd.map(lambda v:json.loads(v)["record"])
         producer.produce_debug("in process.. {}".format(time))
+
+        # get Context for this rdd
         spark = getSparkSessionInstance(rdd.context.getConf())
-        dependencies = {}
-        for d in rule.dependencies:
-            producer.produce_debug("getting dependency for {}".format(d))
-            name = rule.name
-            dependencies[name] = get_table_df(d)
+
+        # get coniguration out of rule
         config = rule.config
+
+        # only expecting one dependency for this rule, so take first item from list by default
+        dependencies = dependencies.get(rule.name)[0]
         try:
-            stream_df = spark.createDataFrame(rdd)
+            #turn current streamdata into dataframe
+            stream_df = spark.createDataFrame(rdd.map(lambda v:json.loads(v)["record"]))
+
+            #validated items are indicated by a successful inner join on the configured columns
             validated = stream_df.join(dependencies[rule.name],on = list(config.keys()))
+
+            #invalid items are indicated by a left outer join on the dependenc table on the configured column
             invalid = stream_df.join(dependencies[rule.name],on = list(config.keys()),how = "left_outer")
+
+            # collect the valid and invalid data and send them to their respective topics
             valid_json = validated.toJSON().collect()
             for data in valid_json:
-                producer.produce_valid(data)
+                producer.send_next(record = data, validity = True, rejectionrule = rule.rejectionrule)
+                #producer.produce_valid(data)
+
             del valid_json
+
             invalid_json = invalid.toJSON().collect()
             for data in invalid_json:
-                producer.produce_reject(data)
+                producer.send_next(record = data, validity = False, rejectionrule = rule.rejectionrule)
+                #producer.produce_reject(data)
+
         except ValueError as e:
-            producer.produce_debug("restart the stream producer! ")
+            producer.produce_debug("restart the stream producer! {}".format(e))
 
-
-    def check_lead_time(time,rdd):
-        rdd  = rdd.map(lambda v:json.loads(v)["record"])
-        producer.produce_debug("in process.. {}".format(time))
-        # should be only one dependency
-        for d in rule.dependencies:
-            producer.produce_debug("getting dependency for {}".format(d))
-            name = rule.name
-            dependency_df = get_table_df(d)
-        config = rule.config
-        config ["leadtime_column"]= "delivery_lead_time"
-        config ["start_column"] = "order_creation_date"
-        config ["end_column"] = "order_expected_delivery"
-        try:
-            stream_df = spark.createDataFrame(rdd)
-            for data in valid_json:
-                producer.produce_valid(data)
-            del valid_json
-            invalid_json = invalid.toJSON().collect()
-            for data in valid_json:
-                producer.produce_invalid(data)
-        except ValueError as e:
-            producer.produce_debug("restart the stream producer! ")
-
-    def check_open_order(time,rdd):
-        rdd  = rdd.map(lambda v:json.loads(v)["record"])
-        producer.produce_debug("in process.. {}".format(time))
-        # should be only one dependency
-        for d in rule.dependencies:
-            producer.produce_debug("getting dependency for {}".format(d))
-            name = rule.name
-            dependency_df = get_table_df(d)
-        config = rule.config
-        config = {}
-        config["status_column"] = "order_status"
-        config["open_order_statuses"] = ["000-000","111-111"]
-        config["open_order_threshold"] = 90
-        try:
-            stream_df = spark.createDataFrame(rdd)
-            for data in valid_json:
-                producer.produce_valid(data)
-            del valid_json
-            invalid_json = invalid.toJSON().collect()
-            for data in valid_json:
-                producer.produce_invalid(data)
-        except ValueError as e:
-            producer.produce_debug("restart the stream producer! ")
-
-
-    validation_functions = {"check_exists":check_exists}
-    validator = validation_functions.get(rule.method)
+    for rule in validation_config:
+        validator = validation_functions.get(rule.method)
     data_ds.foreachRDD(validator)
+
     ssc.start()
     ssc.awaitTermination()
 
