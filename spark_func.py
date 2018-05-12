959eb03d17fd555c9d8d6bc5689d4640d4a54067
Ubuntu <ubuntu@ip-172-31-31-189.us-west-2.compute.internal>
2018-05-12 17:13:24 +0000

    first to upgrade

--- spark_func.py
+++ spark_func.py
@@ -1,16 +1,18 @@
 from helpers.get_data import get_url
 
+
 from pyspark import SparkContext, SQLContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql import Row,SparkSession
-from pyspark.sql.functions import to_json,struct
+from pyspark.sql.functions import to_json,struct, col
 from pyspark.sql.utils import IllegalArgumentException
 from helpers.get_data import get_url
 from pyspark.sql.types import DateType
 from helpers.kafka import KafkaWriter, get_topic, getjsonproducer,getstrproducer
 from config import config
 import json, math, datetime
+import copy
 
 def getSparkSessionInstance(sparkConf):
     if ("sparkSessionSingletonInstance" not in globals()):
@@ -23,7 +25,7 @@ def getSparkSessionInstance(sparkConf):
 def stream_validation(bootstrap_servers,datasource,table,validation_config):
     sc = SparkContext(appName="PythonSparkStreamingKafka")
     sqlc = SQLContext(sc)
-    ssc = StreamingContext(sc,5)
+    ssc = StreamingContext(sc,10)
     jdbc_url , jdbc_properties = get_url(datasource)
 
     #create kafka producer in master
@@ -65,23 +67,26 @@ def stream_validation(bootstrap_servers,datasource,table,validation_config):
         # get Context for this rdd
         spark = getSparkSessionInstance(rdd.context.getConf())
 
-        # get coniguration out of rule
-        config = rule.config
+        ruleconfig = rule.config
+        joinon = ruleconfig.get("join_cols")
+        identcol = ruleconfig.get("identcol")
 
         # only expecting one dependency for this rule, so take first item from list by default
         dependency = dependencies.get(rule.name)[0]
+        dependency.show()
         try:
             #turn current streamdata into dataframe
             stream_df = spark.createDataFrame(rdd.map(lambda v:json.loads(v)["record"]))
+#            producer.produce_debug("joining on {}".format(joinon))
 
             #validated items are indicated by a successful inner join on the configured columns
-            validated = stream_df.join(dependency,on = list(config.keys()))
-
+            validated = stream_df.join(dependency,joinon,"inner")
             #invalid items are indicated by a left outer join on the dependenc table on the configured column
-            invalided = stream_df.join(dependency,on = list(config.keys()),how = "left_outer")
+            invalided = stream_df.join(dependency,joinon,"left_outer").where(col(identcol).isNull())
 
             # collect the valid and invalid data and send them to their respective topics
             valid_json = validated.toJSON().collect()
+#            producer.produce_debug("valid orders {} ".format(len(valid_json)))
             for data in valid_json:
                 producer.send_next(record = data, validity = True, rejectionrule = rule.rejectionrule)
                 #producer.produce_valid(data)
@@ -89,6 +94,7 @@ def stream_validation(bootstrap_servers,datasource,table,validation_config):
             del validated
 
             invalid_json = invalided.toJSON().collect()
+            producer.produce_debug("invalid orders {} ".format(len(invalid_json)))
             for data in invalid_json:
                 producer.send_next(record = data, validity = False, rejectionrule = rule.rejectionrule)
                 #producer.produce_reject(data)
@@ -100,13 +106,10 @@ def stream_validation(bootstrap_servers,datasource,table,validation_config):
 
     def check_lead_time(time,rdd):
         producer.produce_debug("in process.. {}".format(time))
-
-        # get Context for this rdd
+        # get Context for this rdd https://stackoverflow.com/questions/36051299/how-to-subtract-a-column-of-days-from-a-column-of-dates-in-pyspark
         spark = getSparkSessionInstance(rdd.context.getConf())
-
         # get coniguration out of rule
         config = rule.config
-
         # only expecting one dependency for this rule, so take first item from list by default
         dependency = dependencies.get(rule.name)[0]
         try:
@@ -122,42 +125,36 @@ def stream_validation(bootstrap_servers,datasource,table,validation_config):
                     return False
                 else:
                     return True
-
 #            mapped = joined.map(lambda x:mapfunc(x[],x[],x[])).cache()
             #validated items are indicated by a successful inner join on the configured columns
             valildated = mapped.filter(lambda x: x[0] == False)
-
             validated = stream_df.join(dependency,on = list(config.keys()))
-
             #invalid items are indicated by a left outer join on the dependenc table on the configured column
             invalid = stream_df.join(dependency,on = list(config.keys()),how = "left_outer")
-
             # collect the valid and invalid data and send them to their respective topics
             valid_json = validated.toJSON().collect()
             for data in valid_json:
                 producer.send_next(record = data, validity = True, rejectionrule = rule.rejectionrule)
                 #producer.produce_valid(data)
-
             del valid_json
             invalid_json = invalid.toJSON().collect()
             for data in invalid_json:
                 producer.send_next(record = data, validity = False, rejectionrule = rule.rejectionrule)
                 #producer.produce_reject(data)
-            del invalid_json
-
         except ValueError as e:
             producer.produce_debug("restart the stream producer! {}".format(e))
 
     #hardcoded which validation functions are active
     validation_functions = {"check_exists":check_exists }
 
-
     for rule in validation_config:
         validator = validation_functions.get(rule.method)
         data_ds.foreachRDD(validator)
 
+
     ssc.start()
     ssc.awaitTermination()
+    producer.stat_remnants()
 
 
 #        if rule.method == "check_leadtime":
--- stat_consumer.py
+++ stat_consumer.py
@@ -1,14 +1,40 @@
 #! usr/bin/env python3
-import sys
+
 from kafka import KafkaConsumer
 from postgreslib.database_connection import DBConnection
 import os
 import json
+def get_records(query,dbc):
+           response , header = dbc.execute_query(query)
+           records = [{h.name:v for h,v in zip(header,r)} for r in response]
+           return records
+
+def transform_date(dtcol):
+    return dtcol.strftime("%d/%m/%y %H:%M:%S")
+
+def run_statistics(datasource):
+    print("running stats... ")
+    dbc = DBConnection(datasource)
+    dbc.get_base_table_descriptions()
+    query = "select topic_name, min(timestamp) start_time,max(timestamp) end_time, max(quantity) amount from tracked_topics group by topic_name;"
+    records = get_records(query = query,dbc = dbc)
+    runid = get_records("select max(run_id) + 1 newid  from run_stats;",dbc)[0]["newid"]
+    insert_kwargs = {"table":"run_stats"}
+    for record in records:
+        record["elapsed_time"] = (record["end_time"] - record["start_time"]).total_seconds()
+        insert_kwargs.update({"topic_name":record["topic_name"],\
+                              "run_id":runid,\
+                              "start_time":record["start_time"],\
+                              "end_time":record["end_time"],\
+                              "elapsed_time":record["elapsed_time"],\
+                              "amount":record["amount"]})
+        insert_db(dbc,**insert_kwargs)
+    dbc.execute_cursor("delete from tracked_topics where 1 = 1;",commit = True)
+
 
 def insert_db(dbc,**kwargs):
     base_stmt = "insert into {} ({}) values ({});"
     table = kwargs.pop("table")
-    commit = kwargs.pop("commit",False)
     column_definitions = dbc.descriptions.get(table)
     cols = []
     commit_interval = 100
@@ -21,7 +47,7 @@ def insert_db(dbc,**kwargs):
         elif "INT" in column_type.upper():
             val = "{}".format(str(int(data)))
         elif "TIME" in column_type.upper():
-            val = "TIMESTAMP '{}'".format(data.isoformat())
+            val = "TIMESTAMP '{}'".format(data)
 
         else:
             val  = data
@@ -29,7 +55,7 @@ def insert_db(dbc,**kwargs):
     columns = ", ".join(cols)
     values = ", " .join(vals)
     stmt = base_stmt.format(table,columns,values)
-    dbc.execute_cursor(stmt)
+    dbc.execute_cursor(stmt,commit = True)
 
 def consume_stats(bootstrap_servers, datasource):
     dbc = DBConnection(datasource)
@@ -43,19 +69,17 @@ def consume_stats(bootstrap_servers, datasource):
     insert_kwargs.update({"table":"tracked_topics"})
     for i,  message in enumerate(consumer):
         jsonmsg = json.loads(message.value)
-        print(jsonmsg)
-        try:
-            stat_topic = jsonmsg["topic"]
-            stat_count = jsonmsg["count"]
-            stat_time = jsonmsg["timestamp"]
-            insert_kwargs.update({"topic_name":str(stat_topic),\
-                              "quantity":int(stat_count),\
-                              "timestamp":stat_time})
-            insert_db(dbc,**insert_kwargs)
-            if i % 25  == 0:
-                dbc.commit_connection()
-        except:
-            pass
+        stat_topic = jsonmsg["topic"]
+        stat_count = jsonmsg["count"]
+        stat_time = jsonmsg["timestamp"]
+        insert_kwargs.update({"topic_name":str(stat_topic),\
+                          "quantity":int(stat_count),\
+                          "timestamp":stat_time})
+        insert_db(dbc,**insert_kwargs)
+        if i % 100 == 0:
+            print(" inserted i {} tracked records".format(i))
+        if i == 1000000:
+            break
     dbc.commit_connection()
 
 
